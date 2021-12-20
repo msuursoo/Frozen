d609c9ebd9f67a773fef9ca69778802ca6bddf45
Your Name <you@example.com>
2021-12-20 23:59:57 +0200

    frozen

--- Frozen/Program.cs
+++ Frozen/Program.cs
@@ -52,7 +52,7 @@ namespace Frozen
 
         public static string[] GetDataFromFile()
         {
-            string filePath = @"C:\Users\Mart\samples\frozen.txt";
+            string filePath = @"C:\Users...\samples\frozen.txt";
             string[] dataFromFile = File.ReadAllLines(filePath);
 
             return dataFromFile;
